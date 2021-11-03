52323a2f9b7eef4e4817fdc6801a6f948b6f67ba
teamsmartschoolneskar <91049223+teamsmartschoolneskar@users.noreply.github.com>
2021-11-03 08:56:18 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="loginpage.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+<title>LOGIN</title>
 </head>
 <body>
-    
-<br/>
-	<!-- cek pesan notifikasi -->
-	<?php 
-	if(isset($_GET['pesan'])){
-		if($_GET['pesan'] == "gagal"){
-			echo "Login gagal! username dan password salah!";
-		}else if($_GET['pesan'] == "logout"){
-			echo "Anda telah berhasil logout";
-		}else if($_GET['pesan'] == "belum_login"){
-			echo "Anda harus login untuk mengakses halaman admin";
-		}
-	}
-	?>
-	<br/>
+    <h1>SISTEM KESISWAAN SMKN 1 KARAWANG</h1>
 
-    <div class="login-box">
-        <h1>Login Here</h1>
-        <form method="post" action="cek_login.php">
-            <p>Username</p>
-            <input type="text" name="username" placeholder="Enter Username" autocomplete="off">
-            <p>Password</p>
-            <input type="password" name="password" placeholder="Enter Password" autocomplete="off">
-            <a href="#"><input type="submit" name="Submit" value="Sign In"></a>
-            <a href="#">Forgot Password?</a>
-            <a href="#" text-align='right'>Create an Account</a>
+    <?php
+    if(isset($_GET['pesan'])) {
+        if($_GET['pesan']=="gagal"){
+            echo "<div class='alert'>Username dan Password Salah</div>";
+        }
+    }
+    ?>
+    <div class="box-login">
+        <p class="text-login">SILAHKAN LOGIN</p>
+        <form action="cek-login.php" method="POST">
+            <label>Username</label>
+                <input type="text" name="username" class="form_login" placeholder="username" require>
+            <label>Password</label>   
+                <input type="text" name="password" class="form_login" placeholder="password" require>
+            <input type="submit" class="button-login" value="LOGIN">
         </form>
     </div>
     
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
 </body>
 </html>
